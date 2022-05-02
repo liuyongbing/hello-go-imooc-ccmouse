2dd88d1dfe19a8c153becd7818cc8c7ed93de54f
liuyb <liuyb@ickey.cn>
2022-05-03 07:42:57 +0800

    grpc: enum & map
    Change-Id: Ia271b1402698f02d88bc1d998f8c86d160cd4f3c

---
+++ hello_grpc_enum_map/client/client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"google.golang.org/grpc"
+	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
+
+	"github.com/liuyongbing/hello-go-imooc-ccmouse/hello_grpc_enum_map/proto"
+)
+
+func main() {
+	conn, err := grpc.Dial("127.0.0.1:8080", grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	cli := proto.NewGreeterClient(conn)
+	rsp, err := cli.SayHello(context.Background(), &proto.HelloRequest{
+		Name:   "Grpc service",
+		Url:    "http://keyi.art",
+		Gender: proto.GenderEnum_MALE,
+		Mp: map[string]string{
+			"name":    "proto of map",
+			"company": "imooc.com",
+		},
+		AddTime: timestamppb.New(time.Now()),
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(rsp.Message)
+}
---
+++ hello_grpc_enum_map/server/server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"net"
+
+	"google.golang.org/grpc"
+
+	"github.com/liuyongbing/hello-go-imooc-ccmouse/hello_grpc_enum_map/proto"
+)
+
+type Server struct {
+	proto.UnimplementedGreeterServer
+}
+
+func (s *Server) SayHello(ctx context.Context, request *proto.HelloRequest) (*proto.HelloReply, error) {
+	return &proto.HelloReply{
+		Message: "Hello, demo of grpc enum. Name:" + request.Name + " Url:" + request.Url + " Gender:" + request.Gender.String(),
+	}, nil
+}
+
+func main() {
+	g := grpc.NewServer()
+	proto.RegisterGreeterServer(g, &Server{})
+
+	lis, err := net.Listen("tcp", "0.0.0.0:8080")
+	if err != nil {
+		panic("Failed to listen:" + err.Error())
+	}
+	err = g.Serve(lis)
+	if err != nil {
+		panic("Failed to start grpc:" + err.Error())
+	}
+}
