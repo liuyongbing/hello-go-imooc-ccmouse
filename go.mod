2cbb6acd248bfd595e6720a5b5e72380a71da51c
liuyb <liuyb@ickey.cn>
2022-05-10 23:09:15 +0800

    gorm: mysql

--- go.mod
+++ go.mod
@@ -7,4 +7,6 @@ require (
 	github.com/golang/protobuf v1.5.2
 	google.golang.org/grpc v1.45.0
 	google.golang.org/protobuf v1.28.0
+	gorm.io/driver/mysql v1.3.3
+	gorm.io/gorm v1.23.1
 )
---
+++ hello_gorm/ch01/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// 参考 https://github.com/go-sql-driver/mysql#dsn-data-source-name 获取详情
+	dsn := "root:mysql.root@tcp(127.0.0.1:3306)/test_gorm?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+}
