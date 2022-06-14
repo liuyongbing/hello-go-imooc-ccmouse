34d4e72f6bf5a91cac8ea1a4061feeb2d77a855b
liuyb <liuyb@ickey.cn>
2022-06-14 10:06:47 +0800

    hello nacos: fix json tag

--- hello_nacos/config/ServerConfig.go
+++ hello_nacos/config/ServerConfig.go
@@ -5,7 +5,7 @@ type ServerConfig struct {
 	Host        string        `mapstructure:"host" json:"host"`
 	Port        int           `mapstructure:"port" json:"port"`
 	Name        string        `mapstructure:"name" json:"name"`
-	UserSrvInfo UserSrvConfig `mapstructure:"user_srv" json="user_srv"`
-	JWTInfo     JWTConfig     `mapstructure:"jwt" json="jwt"`
-	ConsulInfo  ConsulConfig  `mapstructure:"consul" json="consul"`
+	UserSrvInfo UserSrvConfig `mapstructure:"user_srv" json:"user_srv"`
+	JWTInfo     JWTConfig     `mapstructure:"jwt" json:"jwt"`
+	ConsulInfo  ConsulConfig  `mapstructure:"consul" json:"consul"`
 }
